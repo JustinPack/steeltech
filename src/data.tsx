3493e4b5a371f2efb7111f7c5da1dcec31707918
Justin <justinpack@gmail.com>
2022-05-21 11:29:26 -0600

    Initial commit for website

---
+++ src/data.tsx
@@ -0,0 +1,49 @@
+export const testimonials = [
+    {
+        image: "undefined",
+        name: "undefined",
+        company: "undefined",
+        quote: "undefined",
+    },
+];
+export const technologies = [
+    {
+        entry: "test",
+    },
+];
+
+
+export const projects = [
+    {
+        title: "React Reserve",
+        subtitle: "MERN Stack",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-1.gif",
+        link: "https://google.com",
+    },
+    {
+        title: "React Tracks",
+        subtitle: "React and Python",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-2.gif",
+        link: "https://google.com",
+    },
+    {
+        title: "DevChat",
+        subtitle: "React and Firebase",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-3.gif",
+        link: "https://google.com",
+    },
+    {
+        title: "Epic Todo App",
+        subtitle: "React Hooks",
+        description:
+            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+        image: "./project-4.gif",
+        link: "https://google.com",
+    },
+];
